1e3f268bbc04f118e940fe88b34904333d6a8ea1
Ryan Serra <ryan16serrasilva@gmail.com>
2024-12-15 09:43:02 -0300

    DEV-001 Alocando forma de gerar csv das colunas e os seus valores diferentes

--- internacional.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-import numpy as np 
-df = pd.read_csv("/home/ryan/Downloads/BrFlights2.csv", encoding="ISO-8859-1")
-df_internacional= df.copy()
-df_internacional = df_internacional = df[df["Codigo.Tipo.Linha"] == "Internacional"]
-df_internacional.to_csv("/home/ryan/trabBD3/BrFlights2_internacional.csv", index=False)
-
